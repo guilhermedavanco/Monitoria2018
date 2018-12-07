7f1b8b3bac8830d5d49c8b614001f2e8ea7fb405
guilhermedavanco <guilhermedavanco@yahoo.com.br>
2018-12-07 00:19:16 -0200

    LOOGER_TEST

--- ESP8266/ESP8266.ino
+++ ESP8266/ESP8266.ino
@@ -3,11 +3,12 @@
  ****************************************/
 #include "UbidotsESP8266.h"
 
+
 /****************************************
  * Define Constants
  ****************************************/
 namespace {
-  const char * WIFISSID = "VIVOFIBRA-6249"; // Assign your WiFi SSID
+  const char * WIFISSID = "VIVOFIBRA-6248"; // Assign your WiFi SSID
   const char * PASSWORD = "8DBD1913B2"; // Assign your WiFi password
   const char * TOKEN = "A1E-2XFeEv14k0TsWFzoQxPJyuY18utbw5"; // Assign your Ubidots TOKEN
 }
